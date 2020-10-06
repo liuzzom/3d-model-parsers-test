d2abee9012be0c574260e06b2791175070bb0746
liuzzom <antonino.liuzzo@stud.unifi.it>
2020-10-06 13:05:16 +0200

    suddivisione in funzioni separate

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/webgl-obj.html', function (req, res) {
+    console.log('request for webgl-obj.html');
+    res.sendFile(path.join(__dirname, 'webgl-obj.html'));
+});
+
+app.get('/webgl-mtl.html', function (req, res) {
+    console.log('request for webgl-mtl.html');
+    res.sendFile(path.join(__dirname, 'webgl-mtl.html'));
+});
+
+app.get('/three-obj.html', function (req, res) {
+    console.log('request for three-obj.html');
+    res.sendFile(path.join(__dirname, 'three-obj.html'));
+});
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static('.'));
---
+++ test.js
@@ -0,0 +1,10 @@
+function hello() {
+    {
+        message = "hello";
+        console.log(message);
+    }
+
+    console.log(message);
+}
+
+hello();
